a0ec8484a3a7342ef692fc3ecc298c352c30e02a
Iqra Imtiaz <iqra_imtiaz09@hotmail.com>
2023-02-21 22:58:11 -0500

    adding recipes by ingredients

--- Recipes.py
+++ Recipes.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import ast
+import json
 
 
 class Recipes:
@@ -6,7 +8,32 @@ class Recipes:
     self.df = pd.read_csv (filename)
     
   def getRecipes(self):
-    return self.df.head(10).to_json(orient='records')
+    return list(self.df.head(10).to_dict(orient='records'))
 
-# print 1 recipie below 
-#print (Recipes('data/RAW_recipes.csv').getRecipes())
+  
+  def match_ingredient(self, ingredients, my_ing):
+    if not isinstance(ingredients, list):
+        return False
+    ingredients = [ing.lower() for ing in ingredients]  # convert the ingredients to lowercase
+    num_matches = sum(1 for ing in ingredients if any(item in ing for item in my_ing))
+    return num_matches >= 0.9 * len(my_ing)
+  
+  def getRecipesByIngredients(self, my_ing):
+    my_ing_lower = [ing.lower() for ing in my_ing]
+    my_ing_set = set(my_ing_lower)
+    df = self.df.copy()
+    # convert the string representation of the list of ingredients into an actual list of ingredients
+    df['ingredients'] = df['ingredients'].apply(ast.literal_eval)
+
+    # filter the dataframe based on whether it contains at least 80% of the ingredients in my_ing
+    filtered_df = df[df['ingredients'].apply(self.match_ingredient,args=(my_ing_set,))]
+
+    filtered_df.reset_index(drop=True,inplace=True)
+    return filtered_df.to_json(orient='index')
+  
+# create main for this class
+if __name__ == "__main__":
+  recipes = Recipes('data/RAW_recipes.csv')
+  my_ing = ['salt', 'pepper', 'onion', 'bell pepper', 'potatoes','shallots','parsley','tarragon','olive oil', 
+          'cheese','vinegar']
+  print(recipes.getRecipesByIngredients(my_ing))
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import os
-import json
+from flask import jsonify
 from Recipes import Recipes
+from flask import Response
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
@@ -10,14 +10,20 @@ recipes = Recipes('data/RAW_recipes.csv')
 def index():
     return render_template("index.html")
 
-
 @app.route('/how are you')
 def hello():
     return 'I am good, how about you?'
 
-@app.route('/recipes')
+@app.route('/recipes', methods=['GET'])
 def getRecipes():
-    return json.loads(recipes.getRecipes())
+    return recipes.getRecipes()
+
+@app.route('/recipesByIngredients', methods=['POST'])
+def getRecipesByIngredients():
+    my_ing = request.form['ingredients']
+    my_ing = my_ing.split(",")
+    return Response(recipes.getRecipesByIngredients(my_ing), content_type='application/json')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
